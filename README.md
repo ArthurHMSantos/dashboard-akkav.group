75f401b976ccb9c05228ead0021a1830f0f69dcf
Arthur Henrique <Arthur.profissional20@gmail.com>
2022-12-12 17:20:26 -0300

    first commit dashboard akkav

---
+++ README.md
@@ -0,0 +1,17 @@
+<ol>
+  <li>Instale o python</li>
+
+  <li>Entre no terminal e execute:
+    <code>python -m pip install --upgrade pip</code>
+  </li>
+
+  <li>Baixe este projeto e descompacte o conteudo em uma pasta <code>teste_streamlit</code>. Entre na pasta.</li>
+
+  <li>Execute:
+    <code>pip install -r requirements.txt --upgrade</code>
+  </li>
+
+  <li>Execute:
+    <code>streamlit run app.py</code>
+  </li>
+<ol>
---
+++ app.py
@@ -0,0 +1,99 @@
+import pandas as pd 
+# import seaborn as sns
+import numpy as np
+import streamlit as st
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+
+st.set_page_config(
+    page_title="BankChurners Dashboard",
+    page_icon="🦈",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.markdown("<h1 style='text-align: center;'>BankChurners Dashboard</h1> <p>Utilize o menu lateral para filtrar o dataframe.</p>", unsafe_allow_html=True)
+st.sidebar.title("Filtrar Dataframe")
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """Filter a dataframe using the sidebar."""
+    modify = st.sidebar.checkbox("Adicionar Filtros")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.sidebar.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filtrar quadros com", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 23))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Valores para {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Valores para {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Valores para {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
+
+data_url = "BankChurners.csv"
+
+df = pd.read_csv(data_url)
+st.dataframe(filter_dataframe(df))
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["Gender", "Customer_Age", "Credit_Limit"])
+
+st.bar_chart(chart_data)
+
